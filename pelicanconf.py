c3dccdb809342f47483babf5cd08e04a3a596ab8
Nishanth Ravindran <ravindranno2@gmail.com>
2025-04-01 22:45:38 +0530

    Fix Pelican unable to find config

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+AUTHOR = 'Nishanth'
+SITENAME = "Nishanth's blogs"
+SITEURL = "/"
+THEME = 'pelican-twitchy'
+PATH = "content"
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+
+)
+
+# Social widget
+SOCIAL = (
+    ("Github", "https://github.com/Nishanth-R"),
+    ("LinkedIn", "https://www.linkedin.com/in/nishanth-ravindran-a043475b/")
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True 
